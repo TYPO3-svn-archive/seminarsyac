b2a0b809b6ceaa957f8a2616cf7917dd4dcc9399
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-16 13:34:56 +0000

    Commit the kickstarter-created code.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminarsyac/trunk@35748 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,46 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "seminarsyac".
+#
+# Auto generated 16-07-2010 15:31
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'seminars ke_yac connector',
+	'description' => 'This extension displays events from the "seminars" extension in the ke_yac calendar.',
+	'category' => 'services',
+	'author' => 'Oliver Klee',
+	'author_email' => 'typo3-coding@oliverklee.de',
+	'shy' => '',
+	'dependencies' => 'ke_yac,seminars',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'ke_yac' => '',
+			'seminars' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:9:"ChangeLog";s:4:"aa83";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"4b1e";s:20:"doc/wizard_form.html";s:4:"c553";}',
+);
+
+?>
