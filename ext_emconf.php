f3e5c3efb150a067fd1136d7f3d15d8d9e2d1b7a
oliklee <oliklee@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-17 11:08:47 +0000

    Add a TS setup configuration file.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seminarsyac/trunk@35767 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "seminarsyac".
 #
-# Auto generated 16-07-2010 15:49
+# Auto generated 17-07-2010 13:08
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -42,7 +42,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"aa83";s:39:"class.tx_seminarsyac_EventsProvider.php";s:4:"5acd";s:16:"ext_autoload.php";s:4:"d940";s:12:"ext_icon.gif";s:4:"35fc";s:17:"ext_localconf.php";s:4:"9b77";}',
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"aa83";s:39:"class.tx_seminarsyac_EventsProvider.php";s:4:"7edf";s:16:"ext_autoload.php";s:4:"7a75";s:12:"ext_icon.gif";s:4:"35fc";s:17:"ext_localconf.php";s:4:"9b77";s:14:"ext_tables.php";s:4:"f3ad";s:34:"Configuration/TypoScript/setup.txt";s:4:"7f33";}',
 	'suggests' => array(
 	),
 );
